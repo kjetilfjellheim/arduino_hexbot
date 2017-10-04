d425d8ce44f76614a6ffd5219d18fc2b56b33a51
kjetilfjellheim <kjetil@forgottendonkey.net>
2017-10-04 02:32:37 +0200

    Added positioning info

--- SimpleWalking.X/main.c
+++ SimpleWalking.X/main.c
@@ -6,6 +6,7 @@
 
 #include <xc.h>        /* XC8 General Include File */
 
+#include <stdio.h>
 #include <stdint.h>        /* For uint8_t definition */
 #include <stdbool.h>       /* For true/false definition */
 #include <string.h>
@@ -13,60 +14,75 @@
 #include "system.h"        /* System funct/params, like osc/peripheral config */
 #include "user.h"          /* User funct/params, such as InitApp */
 
+/**
+ * Max length of the movement string.
+ */
+#define MAX_MOVEMENT_STRING_LENGTH 256
+
+#define POSITIONS 4
+
 char UART_Init();
 char UART_Data_Ready();
 char UART_Read();
 void UART_Write(char data);
-void UART_Write_String(const unsigned char *str);
-void MoveInitPosition();
-void Move2462Positions();
-void MoveNextPosition();
-void EmptyMovementString(unsigned char* movementString);
+void UART_Write_String(const char str[]);
+void FillServoPositionString();
+void EmptyMovementString();
+void InitServos();
+
+typedef struct {
+    int verticalMovement[POSITIONS];
+    int horizontalMovement[POSITIONS];
+} Position;
 
 typedef struct {
     unsigned int currentServoPosition;
-    char horizontalServo[2];
-    char upperVerticalServo[2];
-    char lowerVerticalServo[2];
-    unsigned int horizontalMediumPosition;
-    unsigned int verticalUpperMediumPosition;
-    unsigned int verticalLowerMediumPosition;
+    char horizontalServo[3];
+    char upperVerticalServo[3];
+    char lowerVerticalServo[3];
+    int horizontalMediumPosition;
+    int verticalUpperMediumPosition;
+    int verticalLowerMediumPosition;
     int currentHorizontalDelta;
     int currentUpperVerticalDelta;
     int currentLowerVerticalDelta;
+    int multiplierHorizontal;
+    int multiplierVertical;
 } Servoset;
-/**
- * Max length of the movement string.
- */
-const int MAX_MOVEMENT_STRING_LENGTH = 256;
+
 /**
  * Servosets. Each 3 servos(leg) is a set.
  */
-const unsigned int SERVOSETS = 6;
+const int SERVOSETS = 6;
+
+const Position positions[] = {
+      {     
+          {0, 0, 200, 0},
+          {0, 200, 0, -200}
+      }
+ };
 
 /**
  * Current servoinformation for the servosets.
  */
-
-
  const Servoset servosets[] = {
     {
         1, 
-        {'0'},
-        {'1'},
-        {'2'},
-        1500,
-        1300,
+        "12",
+        "1",
+        "2",
+        1800,
+        2200,
         1800,
         0,
         0,
         0
     },
     {
-        2,
-        {'4'},
-        {'5'},
-        {'6'},
+        1,
+        "4",
+        "5",
+        "6",
         1400,
         1600,
         1700,
@@ -75,10 +91,10 @@ const unsigned int SERVOSETS = 6;
         0
     },
     {
-        3,
-        {'8'},
-        {'9'},
-        {'1', '0'},            
+        1,
+        "8",
+        "9",
+        "10",            
         1000,
         1600,
         1800,        
@@ -87,10 +103,10 @@ const unsigned int SERVOSETS = 6;
         0
     },
     {
-        4,
-        {'1', '6'},
-        {'1', '7'},
-        {'1', '8'}, 
+        1,
+        "16",
+        "17",
+        "18", 
         1400,
         1200,
         1500,
@@ -99,10 +115,10 @@ const unsigned int SERVOSETS = 6;
         0
     },
     {
-        5,
-        {'2', '0'},
-        {'2', '1'},
-        {'2', '2'}, 
+        1,
+        "20",
+        "21",
+        "22", 
         2000,
         1300,
         1000,
@@ -111,10 +127,10 @@ const unsigned int SERVOSETS = 6;
         0                
     },
     {
-        6,
-        {'2', '4'},
-        {'2', '5'},
-        {'2', '6'}, 
+        1,
+        "24",
+        "25",
+        "26", 
         1900,
         1400,
         1000,
@@ -126,7 +142,7 @@ const unsigned int SERVOSETS = 6;
 /**
  * Movement string initalized once.
  */
-char movementString[512] = "";
+char movementString[MAX_MOVEMENT_STRING_LENGTH] = "";
 
 void main(void) {    
     
@@ -139,22 +155,22 @@ void main(void) {
      * Initalize.
      */
     InitApp();
-
     /**
      * Init UART.
      */
     UART_Init();
-
+    /**
+     * Init servos to controller by sending #ZPXXXXTYYYY to all servos.
+     */
+    InitServos();
     /**
      * Sets the inital servo positions.
      */
-    MoveInitPosition();
+    FillServoPositionString();
     /**
-     * This moves servos 2, 4 and 6 two positions so that the servos are ready 
-     * for walking.
+     * Transmit positions.
      */
-    Move2462Positions();
-    
+    UART_Write_String(movementString);
     /**
      * Start walking phase. 
      */
@@ -162,7 +178,6 @@ void main(void) {
         /**
          * Change servos to next position.
          */
-        MoveNextPosition();
         __delay_ms(1000);
     }
 
@@ -185,12 +200,13 @@ char UART_Init() {
     return 1;
 }
 
-void UART_Write_String(const unsigned char* str) {
+void UART_Write_String(const char str[]) {
     int arrayLength = strlen(str);
     for (int i = 0; i<arrayLength; i++) {        
-        char a = str[i];
-        if (a != '\0') {
-            UART_Write(a);
+        char a = str[i];        
+        UART_Write(a);
+        if (a == '\r') {
+            return;
         }
     }
 }
@@ -203,23 +219,62 @@ void UART_Write(char data){
 /**
  * Moves all servoes to init position.
  */
-void MoveInitPosition() {
-    EmptyMovementString(movementString);
-    for(unsigned int i = 0; i < SERVOSETS; i++){
-
+void FillServoPositionString() {
+    EmptyMovementString();
+    char tmp[5];
+    unsigned int i = 0;
+    for(i = 0; i < SERVOSETS; i++){
+        strcat(movementString, "#");
+        strcat(movementString, servosets[i].horizontalServo);
+        strcat(movementString, "P");
+        sprintf(tmp, "%d", servosets[i].horizontalMediumPosition);
+        strcat(movementString, tmp);
+        strcat(movementString, "#");
+        strcat(movementString, servosets[i].upperVerticalServo);
+        strcat(movementString, "P");
+        sprintf(tmp, "%d", servosets[i].verticalUpperMediumPosition);
+        strcat(movementString, tmp);  
+        strcat(movementString, "#");
+        strcat(movementString, servosets[i].lowerVerticalServo);
+        strcat(movementString, "P");
+        sprintf(tmp, "%d", servosets[i].verticalLowerMediumPosition);
+        strcat(movementString, tmp);          
     }
+    strcat(movementString, "T2000\r");
 }
 
-void Move2462Positions() {
-    EmptyMovementString(movementString);
+
+void EmptyMovementString() {
+    memset(&movementString[0], 0, sizeof(movementString));
 }
 
-void MoveNextPosition() {
-    EmptyMovementString(movementString);
-}
-
-void EmptyMovementString(unsigned char movementString[]) {
-    for (unsigned int i = 0;i < strlen(movementString); i++) {
-        movementString[i] = '\r';
+void InitServos() {
+    char tmp[5];
+    unsigned int i = 0;
+    for(i = 0; i < SERVOSETS; i++){
+        EmptyMovementString();
+        strcat(movementString, "#");
+        strcat(movementString, servosets[i].horizontalServo);
+        strcat(movementString, "P");
+        sprintf(tmp, "%d", servosets[i].horizontalMediumPosition);
+        strcat(movementString, tmp);
+        strcat(movementString, "\r");
+        UART_Write_String(movementString);
+        EmptyMovementString();
+        strcat(movementString, "#");
+        strcat(movementString, servosets[i].upperVerticalServo);
+        strcat(movementString, "P");
+        sprintf(tmp, "%d", servosets[i].verticalUpperMediumPosition);
+        strcat(movementString, tmp);  
+        strcat(movementString, "\r");
+        UART_Write_String(movementString);
+        EmptyMovementString();
+        strcat(movementString, "#");
+        strcat(movementString, servosets[i].lowerVerticalServo);
+        strcat(movementString, "P");
+        sprintf(tmp, "%d", servosets[i].verticalLowerMediumPosition);
+        strcat(movementString, tmp);         
+        strcat(movementString, "\r");
+        UART_Write_String(movementString);
     }
 }
--- SimpleWalking.X/nbproject/Makefile-XC8_18F87K22.mk
+++ SimpleWalking.X/nbproject/Makefile-XC8_18F87K22.mk
@@ -97,7 +97,7 @@ ${OBJECTDIR}/configuration_bits.p1: configuration_bits.c  nbproject/Makefile-${C
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/configuration_bits.p1.d 
 	@${RM} ${OBJECTDIR}/configuration_bits.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/configuration_bits.p1  configuration_bits.c 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/configuration_bits.p1  configuration_bits.c 
 	@-${MV} ${OBJECTDIR}/configuration_bits.d ${OBJECTDIR}/configuration_bits.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/configuration_bits.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
@@ -105,7 +105,7 @@ ${OBJECTDIR}/interrupts.p1: interrupts.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/interrupts.p1.d 
 	@${RM} ${OBJECTDIR}/interrupts.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/interrupts.p1  interrupts.c 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/interrupts.p1  interrupts.c 
 	@-${MV} ${OBJECTDIR}/interrupts.d ${OBJECTDIR}/interrupts.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/interrupts.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
@@ -113,7 +113,7 @@ ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/main.p1.d 
 	@${RM} ${OBJECTDIR}/main.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
 	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
@@ -121,7 +121,7 @@ ${OBJECTDIR}/system.p1: system.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/system.p1.d 
 	@${RM} ${OBJECTDIR}/system.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/system.p1  system.c 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/system.p1  system.c 
 	@-${MV} ${OBJECTDIR}/system.d ${OBJECTDIR}/system.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/system.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
@@ -129,7 +129,7 @@ ${OBJECTDIR}/user.p1: user.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
 	@${RM} ${OBJECTDIR}/user.p1.d 
 	@${RM} ${OBJECTDIR}/user.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/user.p1  user.c 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib $(COMPARISON_BUILD)  --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/user.p1  user.c 
 	@-${MV} ${OBJECTDIR}/user.d ${OBJECTDIR}/user.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/user.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
@@ -187,7 +187,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/SimpleWalking.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) --chip=$(MP_PROCESSOR_OPTION) -G -mdist/${CND_CONF}/${IMAGE_TYPE}/SimpleWalking.X.${IMAGE_TYPE}.map  -D__DEBUG=1 --debugger=none  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"        $(COMPARISON_BUILD) --memorysummary dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -odist/${CND_CONF}/${IMAGE_TYPE}/SimpleWalking.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
+	${MP_CC} $(MP_EXTRA_LD_PRE) --chip=$(MP_PROCESSOR_OPTION) -G -mdist/${CND_CONF}/${IMAGE_TYPE}/SimpleWalking.X.${IMAGE_TYPE}.map  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=+asm,+asmfile,-speed,+space,-debug,-local --addrqual=ignore --mode=free -P -N255 --warn=-3 --asmlist -DXPRJ_XC8_18F87K22=$(CND_CONF)  --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,-plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"     --rom=default,-3dc0-3fff --ram=default,-2f4-2ff,-f9c-f9c,-fd4-fd4,-fdb-fdf,-fe3-fe7,-feb-fef,-ffd-fff  $(COMPARISON_BUILD) --memorysummary dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml -odist/${CND_CONF}/${IMAGE_TYPE}/SimpleWalking.X.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}     
 	@${RM} dist/${CND_CONF}/${IMAGE_TYPE}/SimpleWalking.X.${IMAGE_TYPE}.hex 
 	
 else
